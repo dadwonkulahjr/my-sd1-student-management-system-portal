f2e4a7089b350b408e376e11d97ab112e5111441
dadwonkulahjr <dad.wonkulah@comaxict.com>
2021-12-14 23:25:44 +0000

    Remove namespace that is not needed in startup.cs file.

--- MySD1ManagementSystemStudentPortalWeb/Startup.cs
+++ MySD1ManagementSystemStudentPortalWeb/Startup.cs
@@ -1,17 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MySD1ManagementSystemStudentPortalWeb.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace MySD1ManagementSystemStudentPortalWeb
 {
@@ -24,7 +19,6 @@ namespace MySD1ManagementSystemStudentPortalWeb
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -37,7 +31,6 @@ namespace MySD1ManagementSystemStudentPortalWeb
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -48,7 +41,6 @@ namespace MySD1ManagementSystemStudentPortalWeb
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
