7f4e2821b39f55d13a1d41d93432e3b71322efa1
dadwonkulahjr <dad.wonkulah@comaxict.com>
2021-12-16 23:38:58 +0000

    Added the gender model

--- MySD1ManagementSystemStudentPortalWeb/Models/Student.cs
+++ MySD1ManagementSystemStudentPortalWeb/Models/Student.cs
@@ -1,11 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MySD1ManagementSystemStudentPortalWeb.Models
 {
     public class Student
     {
+        [Key]
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
     }
 }
