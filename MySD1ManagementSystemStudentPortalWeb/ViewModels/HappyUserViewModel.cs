87dbd80087a4bc9e5ef8aff34136115bd505be32
dadwonkulahjr <dad.wonkulah@comaxict.com>
2021-12-16 06:04:26 +0000

    dll files! and added a viewmodel file.

--- MySD1ManagementSystemStudentPortalWeb/ViewModels/HappyUserViewModel.cs
+++ MySD1ManagementSystemStudentPortalWeb/ViewModels/HappyUserViewModel.cs
@@ -7,5 +7,8 @@ namespace MySD1ManagementSystemStudentPortalWeb.ViewModels
 {
     public class HappyUserViewModel
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
     }
 }
