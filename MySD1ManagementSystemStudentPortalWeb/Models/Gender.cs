53466207dbedbc20f424d60575954cc812ba8a38
dadwonkulahjr <dad.wonkulah@comaxict.com>
2021-12-16 23:40:27 +0000

    Added the assignment model

--- MySD1ManagementSystemStudentPortalWeb/Models/Gender.cs
+++ MySD1ManagementSystemStudentPortalWeb/Models/Gender.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MySD1ManagementSystemStudentPortalWeb.Models
 {
     public class Gender
     {
+        [Key]
+        public int GenderId { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
     }
 }
