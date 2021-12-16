9390431777adbc43648b330e4ad32b12e61052f2
dadwonkulahjr <dad.wonkulah@comaxict.com>
2021-12-16 06:02:46 +0000

    dll files!

---
+++ MySD1ManagementSystemStudentPortalWeb/ViewModels/HappyUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySD1ManagementSystemStudentPortalWeb.ViewModels
+{
+    public class HappyUserViewModel
+    {
+    }
+}
